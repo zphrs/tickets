58234e84f86428e9f3d37cd7bf2c50980dc5b86b
zphyrj <46908127+zphyrj@users.noreply.github.com>
2022-04-05 02:56:17 -0700

    added custom icon

--- dist/sw.js
+++ dist/sw.js
@@ -1,66 +1,66 @@
-CACHE_NAME = 'mvhs-timer';
-CACHE_VERSION = 'V2';
+CACHE_NAME = "tickets"
+CACHE_VERSION = "V3"
 
-self.addEventListener('install', function(event) {
-  self.skipWaiting();
-  event.waitUntil( async () => {
+self.addEventListener("install", function (event) {
+  self.skipWaiting()
+  event.waitUntil(async () => {
     if (self.registration.navigationPreload) {
       // Enable navigation preloads!
-      await self.registration.navigationPreload.enable();
+      await self.registration.navigationPreload.enable()
     }
-    await caches.open(CACHE_NAME + CACHE_VERSION).then(function(cache) {
-      return cache.addAll([
-        '/mvhs-timer/',
-        '/mvhs-timer/index.html',
-      ]);
+    await caches.open(CACHE_NAME + CACHE_VERSION).then(function (cache) {
+      return cache.addAll(["/tickets/", "/tickets/index.html"])
     })
-  });
-});
+  })
+})
 
-self.addEventListener('activate', () => {
-  caches.keys().then(function(keyList) {
-    console.log(keyList);
-    keyList.forEach(key=> caches.delete(key));
-  });
-});
+self.addEventListener("activate", () => {
+  caches.keys().then(function (keyList) {
+    console.log(keyList)
+    keyList.forEach(key => caches.delete(key))
+  })
+})
 
-
-self.addEventListener('fetch', function(event) {
-
+self.addEventListener("fetch", function (event) {
   function getCachedResponse(url) {
     return caches.match(url).then(function (response) {
       if (response) {
-        return response;
+        return response
       }
-      return new Response('Service worker fetch error', {
+      return new Response("Service worker fetch error", {
         status: 500,
-        statusText: 'Service worker fetch error'
-      });
-    });
+        statusText: "Service worker fetch error",
+      })
+    })
   }
 
   function getFromServer(url) {
-    const response = fetch(url)
-      .then((response) => {
-        if (!response || response.status !== 200) {
-          console.log('loading from cache due to bad response from server ' + event.request.url, response);
-          return caches.match(url);
-        }
-        return response;
-      })
-    return response;
+    const response = fetch(url).then(response => {
+      if (!response || response.status !== 200) {
+        console.log(
+          "loading from cache due to bad response from server " +
+            event.request.url,
+          response
+        )
+        return caches.match(url)
+      }
+      return response
+    })
+    return response
   }
 
-  event.respondWith((async () => {
-    const url = event.request.url;
-    const cachedResponse = await getCachedResponse(url);
-    if (cachedResponse && cachedResponse.status === 200) {
-      return cachedResponse;
-    }
-    const serverResponse = await getFromServer(url);
-    const cache = await caches.open(CACHE_NAME + CACHE_VERSION);
-    const clonedResponse = serverResponse.clone();
-    cache.put(url, clonedResponse);
-    return serverResponse;
-  })());
-});
+  event.respondWith(
+    (async () => {
+      const url = event.request.url
+      const cachedResponse = await getCachedResponse(url)
+      if (cachedResponse && cachedResponse.status === 200) {
+        return cachedResponse
+      }
+      const serverResponse = await getFromServer(url)
+      const cache = await caches.open(CACHE_NAME + CACHE_VERSION)
+      const clonedResponse = serverResponse.clone()
+      cache.put(url, clonedResponse)
+      return serverResponse
+    })()
+  )
+})
